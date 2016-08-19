bd3d878bc5ffc1912dd8577d848fbe1e79352253
highdutch <dick.heijink@gmail.com>
2016-08-19 22:10:43 +0200

    Version bump

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 "use strict";
 
 const path = require('path');
-const pkg = require('./package')
-const setup = pkg[pkg.name];
+const pkg = require('./package');
 
 const analyze = require('oma-analyze');
 const archive = require('oma-archive');
 const bundle = require('oma-bundle');
+const bootstrap = require('oma-bootstrap');
 const constants = require('oma-constants');
 const imagine = require('oma-imagine');
 const util = require('oma-util');
@@ -73,30 +73,11 @@ module.exports = {
       silent: silentJavaScript
     },
     command: opts => {
-      const error = opts.silent ? null : console.error;
-      const destination = opts[''][0];
-      const archiveDir = `${destination}/${constants.library.preserve}`;
-      const bundleDir = `${destination}/${constants.library.publish}`;
-      return (opts.clean ? util.rmdir(destination) : Promise.resolve())
-        .then(() => Promise.all(setup.bootstrap.map(name => {
-          const sourceDir = path.dirname(require.resolve(name));
-          const version = require(`${sourceDir}/package`).version;
-          const archivePath = `${archiveDir}/${name}/${version}/${constants.archive.file}.zip`;
-          return util.stat(archivePath)
-            .catch(() => util.openWriteStream(archivePath)
-              .then(output => archive(error, [sourceDir], output)))
-            .then(() => archivePath)
-            ;
-        })))
-        .then(archivePaths => Promise.all(archivePaths.map(archivePath => {
-          const analysisPath = `${path.dirname(archivePath)}/${constants.archive.file}.json`;
-          return Promise.all([
-            bundle(archivePath, bundleDir),
-            util.stat(analysisPath)
-              .catch(() => util.copyJSON(analyze(archivePath), util.openWriteStream(analysisPath)))
-          ]);
-        })))
-        ;
+      const archives = {};
+      pkg[pkg.name].bootstrap.forEach(name => {
+        archives[name] = path.dirname(require.resolve(name));
+      });
+      return bootstrap(archives, opts);
     }
   },
   bundle: {

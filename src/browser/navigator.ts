20882ba01513d57073428def3f939128040419ac
unknown <open.mainframe@gmail.com>
2017-06-07 20:03:07 +0200

    Version bump

--- src/browser/navigator.ts
+++ src/browser/navigator.ts
@@ -1,14 +1,5 @@
-import * as always from 'oma/always'
 import * as render from 'oma/dom/render'
 
-const bla = document.createElement('a')
-
-const foo = sessionStorage.getItem('oma-navigator')
-
-const shitje = render.bar()
-
-const wtf = always.returnNothing()
-
 export function bar() {
-  return bla
+  return render.bar()
 }
